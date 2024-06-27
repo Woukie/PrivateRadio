14fe47eff4e13fb7cd9ce0da14f90066e66baafc
Woukie <lucaseldon@live.com>
2024-06-27 16:01:03 +0100

    Revert "Added titles"
    This reverts commit 326052a938fa8101383a2caa0d22c96605e364dc.

--- private_radio/lib/src/dashboard/app_bar.dart
+++ private_radio/lib/src/dashboard/app_bar.dart
@@ -4,59 +4,53 @@ class TopBar extends StatelessWidget {
   const TopBar({
     super.key,
     required this.searchController,
+    required this.boxDecoration,
     required this.hidden,
   });
 
   final TextEditingController searchController;
+  final BoxDecoration boxDecoration;
   final bool hidden;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      bottom: false,
-      child: AnimatedSize(
-        alignment: Alignment.topLeft,
-        duration: const Duration(milliseconds: 250),
-        child: Column(
-          children: [
-            AnimatedSwitcher(
-              switchInCurve: Curves.easeInOut,
-              switchOutCurve: Curves.easeInOut,
-              duration: const Duration(milliseconds: 250),
-              transitionBuilder: (child, animation) {
-                return FadeTransition(
+    return AnimatedSize(
+      duration: const Duration(milliseconds: 250),
+      child: Container(
+        decoration: boxDecoration,
+        child: SafeArea(
+          bottom: false,
+          child: AnimatedSwitcher(
+            switchInCurve: Curves.easeInOut,
+            switchOutCurve: Curves.easeInOut,
+            duration: const Duration(milliseconds: 250),
+            transitionBuilder: (child, animation) {
+              return FadeTransition(
                   opacity: animation,
-                  child: child,
-                );
-              },
-              child: hidden
-                  ? Container()
-                  : Card(
-                      color:
-                          Theme.of(context).colorScheme.surfaceContainerHighest,
-                      margin: const EdgeInsets.all(6),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12),
-                        child: TextField(
-                          controller: searchController,
-                          decoration: const InputDecoration(
-                            contentPadding: EdgeInsets.zero,
-                            border: InputBorder.none,
-                            icon: Icon(Icons.search),
-                          ),
+                  child: SizeTransition(
+                    sizeFactor: animation,
+                    child: child,
+                  ));
+            },
+            child: hidden
+                ? Container()
+                : Card(
+                    color:
+                        Theme.of(context).colorScheme.surfaceContainerHighest,
+                    margin: const EdgeInsets.all(6),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: TextField(
+                        controller: searchController,
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.zero,
+                          border: InputBorder.none,
+                          icon: Icon(Icons.search),
                         ),
                       ),
                     ),
-            ),
-            AnimatedSwitcher(
-              switchInCurve: Curves.easeInOut,
-              switchOutCurve: Curves.easeInOut,
-              duration: const Duration(milliseconds: 250),
-              transitionBuilder: (child, animation) {
-                return FadeTransition(opacity: animation, child: child);
-              },
-            ),
-          ],
+                  ),
+          ),
         ),
       ),
     );
