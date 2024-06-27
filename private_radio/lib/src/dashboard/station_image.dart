6697f1b58952104d60406114da6dc287a3917050
Woukie <lucaseldon@live.com>
2024-06-27 10:31:28 +0100

    Updated image rendering

--- private_radio/lib/src/dashboard/station_image.dart
+++ private_radio/lib/src/dashboard/station_image.dart
@@ -5,19 +5,41 @@ class StationImage extends StatelessWidget {
   const StationImage({
     super.key,
     required this.path,
-    this.size,
+    this.size = 80,
   });
 
   final String path;
-  final double? size;
+  final double size;
 
   @override
   Widget build(BuildContext context) {
     return Image(
       height: size,
       width: size,
-      errorBuilder: (context, error, stackTrace) =>
-          const Icon(size: 52, Icons.error),
+      errorBuilder: (context, error, stackTrace) => Card(
+        margin: EdgeInsets.zero,
+        color: Theme.of(context).colorScheme.surfaceContainerHighest,
+        child: Icon(
+          size: size,
+          Icons.radio,
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+      ),
+      loadingBuilder: (context, child, loadingProgress) => SizedBox(
+        width: size,
+        height: size,
+        child: Stack(
+          alignment: Alignment.center,
+          fit: StackFit.expand,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(12.0),
+              child: CircularProgressIndicator(),
+            ),
+            child,
+          ],
+        ),
+      ),
       image:
           path.startsWith("http") ? NetworkImage(path) : FileImage(File(path)),
     );
